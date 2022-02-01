11aede125c73574d174102e24f0eeb96d1fbde5a
Kayleigh324 <kayleightuppen@gmail.com>
2022-02-01 20:58:20 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Dimensions,
+  StyleSheet, 
+  Text, 
+  TouchableWithoutFeedback, 
+  Image, 
+  SafeAreaView, 
+  Alert, 
+  View, 
+  Button,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  const handlePress = () => console.log('Text pressed')
+  console.log(Dimensions.get('screen'));
+  
+ return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: '50%',
+          height: 70,
+        }}
+      
+      ></View>
+      <Button 
+        title='Click Me' 
+        onPress={() => 
+          Alert.alert('My title', 'My Message', [
+            { text: 'Yes', onPress: () => console.log('Yes pressed') },
+            { text: 'No' },
+          ])
+        } />
+      
+      <Text onPress={handlePress}>Hello!!</Text>
+      <TouchableWithoutFeedback onPress={() => console.log('Image pressed')}>
+      <Image 
+      fadeDuration={1000}
+      source={{
+        width: 200,
+        height: 300,
+        uri: 'https://reactnative.dev/img/tiny_logo.png',
+      }}/>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
